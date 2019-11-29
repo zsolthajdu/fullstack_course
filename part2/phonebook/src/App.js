e5dcd583ad9f2c20418720d62512ac55ba0551a7
Zsolt Hajdu <z_hajdu@yahoo.com>
2019-11-28 20:08:07 -0800

    Phonebook exercise : using backend database as data source.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123 3456' },
-    { name: 'Ada Lovelace', number: '011 39-44-5323523' },
-    { name: 'Dan Abramov', number: '011 12-43-234345' },
-    { name: 'Mary Poppendieck', number: '011 39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+
+  useEffect( () => {
+    axios
+      .get( 'http://localhost:3001/persons')
+      .then( response => {
+          setPersons( response.data )  
+      })
+  }, [] )
 
   const handleNameChange = (event) => {
     setNewName( event.target.value )
@@ -44,7 +48,7 @@ const App = () => {
     else {
       window.alert( `${newName} is already added to phonebook.` )
     }
-  }  
+  }
 
   return (
     <div>
